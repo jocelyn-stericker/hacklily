76129c39e1811c2e7f4bf5649bb093a60c77aa49
Josh Netterfield <joshua@nettek.ca>
2015-05-18 09:39:27 -0700

    Beams: deal with poor beam formats.
    Note: I'm not sure if 99a was invalid, or if we need to fix the beam
    processor to detect implicit hooks.

--- makelily/src/models/postprocessors/beam.ts
+++ makelily/src/models/postprocessors/beam.ts
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+"use strict";
+
 import MusicXML                 = require("musicxml-interfaces");
 import _                        = require("lodash");
 import invariant                = require("react/lib/invariant");
@@ -28,6 +30,8 @@ interface IMutableBeam {
     initial: MusicXML.Beam;
 }
 
+type BeamSet = {[voice: string]: IMutableBeam[]};
+
 /** 
  * Lays out measures within a bar & justifies.
  * 
@@ -38,7 +42,7 @@ function beam(options: Engine.Options.ILayoutOptions, bounds: Engine.Options.ILi
     _.forEach(measures, measure => {
         // Note that the `number` property of beams does NOT differentiate between sets of beams,
         // as it does with e.g., ties. See `note.mod`.
-        let activeBeams: {[voice: string]: IMutableBeam[]} = {};
+        let activeBeams: BeamSet = {};
         // Invariant: measure.elements[i].length == measure.elements[j].length for all valid i, j.
         _.times(measure.elements[0].length, i => {
             _.forEach(measure.elements, elements => {
@@ -49,7 +53,25 @@ function beam(options: Engine.Options.ILayoutOptions, bounds: Engine.Options.ILi
                 }
                 let chord: Engine.IChord = <any> model;
                 let noteWithBeams = _.find(chord, el => !!el.beams);
+                if (noteWithBeams && noteWithBeams.grace) {
+                    // TODO: grace notes
+                    return;
+                }
                 if (!noteWithBeams) {
+                    _.forEach(chord, note => {
+                        if (!note || note.grace) {
+                            // TODO: grace notes
+                            return;
+                        }
+                        _.forEach(activeBeams[note.voice], (beam, idx) => {
+                            if (!beam) {
+                                return;
+                            }
+                            console.warn("Beam in voice %s, level %s was not explicitly closed " +
+                                "before another note was added.", note.voice, idx);
+                            terminateBeam$(note.voice, idx, activeBeams);
+                        });
+                    });
                     return;
                 }
                 let beams = noteWithBeams.beams;
@@ -64,7 +86,8 @@ function beam(options: Engine.Options.ILayoutOptions, bounds: Engine.Options.ILi
                         case MusicXML.BeamType.ForwardHook:
                             activeBeams[voice] = activeBeams[voice] || [];
                             if (activeBeams[voice][idx]) {
-                                console.warn("Should not create beam at level %s in voice %s because it already exists", idx, voice);
+                                console.warn("Beam at level %s in voice %s should have been closed before being opened again.", idx, voice);
+                                terminateBeam$(voice, idx, activeBeams);
                             }
                             activeBeams[voice][idx] = {
                                 number: idx,
@@ -87,18 +110,30 @@ function beam(options: Engine.Options.ILayoutOptions, bounds: Engine.Options.ILi
                             invariant(idx in activeBeams[voice], "Cannot end non-existant " +
                                 "beam (no beam at level %s in voice %s)", idx, voice);
                             activeBeams[voice][idx].elements.push(layout);
-                            // console.log("Would now process ", activeBeams[voice][idx]);
-                            _.forEach(activeBeams[voice][idx].elements, eLayout => {
-                                (<Engine.IChord><any>eLayout.model)[0].printObject = false;
-                            });
-                            delete activeBeams[voice][idx];
+                            terminateBeam$(voice, idx, activeBeams);
                             break;
                     }
                 });
             });
         });
+        _.forEach(activeBeams, (beams, voice) => {
+            _.forEach(beams, (beam, idx) => {
+                if (!beam) {
+                    return;
+                }
+                console.warn("Beam in voice %s, level %s was not closed before the end of the measure.", voice, idx);
+                terminateBeam$(parseInt(voice, 10), idx, activeBeams);
+            });
+        });
     });
     return measures;
+}
+
+function terminateBeam$(voice: number, idx: number, beamSet$: BeamSet) {
+    _.forEach(beamSet$[voice][idx].elements, eLayout => {
+        (<Engine.IChord><any>eLayout.model)[0].printObject = false;
+    });
+    delete beamSet$[voice][idx];
 }
 
 export = beam;
